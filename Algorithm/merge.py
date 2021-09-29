24b656af6567a120bfd79c1d20a8007825175e8f
chjy0202 <chjy0202@naver.com>
2021-09-30 07:28:02 +0900

    merge2

--- Algorithm/merge.py
+++ Algorithm/merge.py
@@ -1,11 +1,16 @@
+import random
+
+
 def split(data):
 	medium = len(data) // 2
 	left = data[:medium]
 	right = data[medium:]
-	print(left,right)
+	print(left, right)
 
-data_list = [3,4,1,3,2]
+
+# data_list = [3, 4, 1, 3, 2]
 # split(data_list)
+
 
 def merge_split(data):
 	if len(data) <= 1:
@@ -13,11 +18,37 @@ def merge_split(data):
 	medium = len(data) // 2
 	left = merge_split(data[:medium])
 	right = merge_split(data[medium:])
-	# print(left,right)
+	# print(left, right)
 	return merge(left, right)
 
-data_list = [3,4,1,3,2,6,7,8]
-merge_split(data_list)
 
- 
-  
+# data_list = [3, 4, 1, 3, 2, 6, 7, 8]
+# merge_split(data_list)
+
+def merge(left, right):
+	merged = list()
+	lp, rp = 0, 0
+
+	while len(left) > lp and len(right) > rp:
+		if left[lp] > right[rp]:
+			merged.append(right[rp])
+			rp += 1
+		else:
+			merged.append(left[lp])
+			lp += 1
+
+	while len(left) > lp:
+		merged.append(left[lp])
+		lp += 1
+	while len(right) > rp:
+		merged.append(right[rp])
+		rp += 1
+
+	# print(merged)
+	return merged
+
+data_list = random.sample(range(100),10)
+# print(data_list)
+
+print(merge_split(data_list))
+
---
+++ main.py
@@ -0,0 +1,57 @@
+import random
+
+
+def split(data):
+	medium = len(data) // 2
+	left = data[:medium]
+	right = data[medium:]
+	print(left, right)
+
+
+# data_list = [3, 4, 1, 3, 2]
+# split(data_list)
+
+
+def merge_split(data):
+	if len(data) <= 1:
+		return data
+	medium = len(data) // 2
+	left = merge_split(data[:medium])
+	right = merge_split(data[medium:])
+	# print(left, right)
+	return merge(left, right)
+
+
+# data_list = [3, 4, 1, 3, 2, 6, 7, 8]
+# merge_split(data_list)
+
+def merge(left, right):
+	merged = list()
+	lp, rp = 0, 0
+
+	while len(left) > lp and len(right) > rp:
+		if left[lp] > right[rp]:
+			merged.append(right[rp])
+			rp += 1
+		else:
+			merged.append(left[lp])
+			lp += 1
+
+	# while len(left) > lp:
+	# 	merged + left[lp:]
+	# 	break
+	# while len(right) > rp:
+	# 	merged + right[rp:]
+	# 	break
+
+	print(merged)
+	return merged
+
+data_list = random.sample(range(100),10)
+print(data_list)
+
+print(merge_split(data_list))
+
+# listd = [1,2,3,4,5]
+# asd = [4,5,6,7]
+# print(listd + asd[2:])
