fd7f35acd3352a8a8fa1abf6755cea7401141eef
chjy0202 <chjy0202@naver.com>
2022-03-18 23:02:39 +0900

    버블정렬_동작방식update

--- Algorithm/bubble.py
+++ Algorithm/bubble.py
@@ -1,14 +1,19 @@
 # 버블정렬
 
+for index in range(데이터길이 - 1):
+  for index2 in range(데이터길이 - index - 1):
+    if 앞데이터 > 뒷데이터:
+      swap(앞데이터, 뒷데이터)
+
 def bubblesort(data):
   for index in range(len(data) - 1):
     swap = False
     for index2 in range(len(data) - index - 1):
       if data[index2] > data[index2 + 1]:
-        data[index2], data[index2 + 1] = data[index2 + 1],data[index2]
+        data[index2], data[index2 + 1] = data[index2 + 1], data[index2]
         swap = True # 턴이 실행이 될 때 마다 true로 바뀐다. 
     if swap == False:
-      break
+      break # 스왑이 일어나지 않았으니 이미 정렬이 완료된 상태
   return data
 
 
