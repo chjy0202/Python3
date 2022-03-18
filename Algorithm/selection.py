d40efd27fc7c2ab1a5e29606bfa744deccad1f29
chjy0202 <chjy0202@naver.com>
2022-03-18 23:34:30 +0900

    선택정렬_동작방식update

--- Algorithm/selection.py
+++ Algorithm/selection.py
@@ -1,11 +1,19 @@
 # 선택정렬
 
+for stand in range(데이터길이 - 1):
+	lowest = stand
+	for idx in range(stand + 1, 데이터길이):
+		if data[idx] < data[lowest]:
+			lowest = idx
+	swap(data[stand], data[lowest])
+
 def selection_sort(data):
 	for stand in range(len(data) - 1):
-		lowest = stand
-		for index in range(stand+1, len(data)):
+		lowest = stand # 맨 앞의 인덱스로 초기화
+		for index in range(stand + 1, len(data)):
 			if data[lowest] > data[index]:
 				lowest = index
+		# 한 번 턴을 돌면 최솟값의 인덱스가 lowest에 담겨있고 이를 맨 앞과 바꿔준다.
 		data[lowest], data[stand] = data[stand], data[lowest]
 	return data
 
