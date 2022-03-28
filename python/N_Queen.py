41d4a7ec23893dceda877e8f9794fe32cda91929
chjy0202 <chjy0202@naver.com>
2022-03-28 22:52:03 +0900

    N_Queen_리턴값수정

--- python/N_Queen.py
+++ python/N_Queen.py
@@ -5,24 +5,21 @@ def possible(visit, col):
             return False
     return True
 
-# 파라미터 : n, 현재 행, 결과, 방문한 퀸의 행 번호가 담긴 리스트
-def DFS(n, row, answer, visit):
+# 파라미터 : n, 현재 행, 방문한 퀸의 행 번호가 담긴 리스트
+def DFS(n, row, visit):
+    answer = 0
     if row == n:
-        answer += 1
-        print(visit)
-        print(answer)
-        return answer
-
+        return 1
+    
     # col : 현재 열
     for col in range(n):
         # 퀸을 이동할 수 없는 자리가 맞다면
         if possible(visit, col):
             visit.append(col)
-            DFS(n, row + 1, answer, visit)
+            answer += DFS(n, row + 1, visit)
             # 이동할 수 없는 자리가 없을 때
             visit.pop()
+    return answer
 
 def solution(n):
-    answer = 0
-    DFS(n, 0, answer, [])
-    return answer
+    return DFS(n, 0, [])
