f3d9d00dd36e1ac6e42373313047aefd25973275
chjy0202 <chjy0202@naver.com>
2022-03-31 23:48:25 +0900

    병합정렬_update

--- Algorithm/merge.py
+++ Algorithm/merge.py
@@ -1,6 +1,6 @@
 import random
 
-
+# 리스트를 자르는 함수
 def split(data):
 	medium = len(data) // 2
 	left = data[:medium]
@@ -13,12 +13,12 @@ def split(data):
 
 
 def merge_split(data):
+	# 데이터 길이가 1일때 까지 재귀적으로 함수를 계속 불러온다.
 	if len(data) <= 1:
 		return data
 	medium = len(data) // 2
 	left = merge_split(data[:medium])
 	right = merge_split(data[medium:])
-	# print(left, right)
 	return merge(left, right)
 
 
@@ -29,6 +29,7 @@ def merge(left, right):
 	merged = list()
 	lp, rp = 0, 0
 
+	# case1 : left, right 가 아직 남아있을 때
 	while len(left) > lp and len(right) > rp:
 		if left[lp] > right[rp]:
 			merged.append(right[rp])
@@ -37,14 +38,16 @@ def merge(left, right):
 			merged.append(left[lp])
 			lp += 1
 
+	# case2 : left 만 남아있을 때
 	while len(left) > lp:
 		merged.append(left[lp])
 		lp += 1
+
+	# case3 : right 만 남아 있을 때 
 	while len(right) > rp:
 		merged.append(right[rp])
 		rp += 1
 
-	# print(merged)
 	return merged
 
 data_list = random.sample(range(100),10)
@@ -52,3 +55,4 @@ data_list = random.sample(range(100),10)
 
 print(merge_split(data_list))
 
+# 시간복잡도 O(n logn)
